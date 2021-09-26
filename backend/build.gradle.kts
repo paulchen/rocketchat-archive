af128fca370ab173b38f2d152c86d69609b58d6f
Paul Staroch <paulchen@rueckgr.at>
2021-09-26 23:43:15 +0200

    upgrade dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,12 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val ktor_version = "1.6.1"
+val ktor_version = "1.6.3"
 val log4jVersion = "2.14.1"
 
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm") version "1.5.31"
     application
-    id("com.palantir.docker") version "0.26.0"
+    id("com.palantir.docker") version "0.29.0"
     id("com.palantir.git-version") version "0.12.3"
 }
 
@@ -26,7 +26,7 @@ sourceSets {
 }
 
 dependencies {
-    implementation("org.litote.kmongo:kmongo:4.2.8")
+    implementation("org.litote.kmongo:kmongo:4.3.0")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("io.ktor:ktor-gson:$ktor_version")
     implementation("io.ktor:ktor-jackson:$ktor_version")
@@ -36,9 +36,9 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
     implementation("org.slf4j:slf4j-api:1.7.32")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.4")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.5")
 
-    testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.5.31")
 }
 
 tasks.test {
