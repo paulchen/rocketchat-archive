9308cb3815ca8bf2e1a4c422a21754269c023413
Paul Staroch <paulchen@rueckgr.at>
2022-03-27 20:38:16 +0200

    properly handle multi-line messages

--- backend/src/main/kotlin/at/rueckgr/rocketchat/archive/MessageProcessor.kt
+++ backend/src/main/kotlin/at/rueckgr/rocketchat/archive/MessageProcessor.kt
@@ -6,8 +6,15 @@ object MessageProcessor {
     private val URL_PATTERN = """(?:(?:ftp|http)[s]*://|www\.)[^.]+\.[^ \n")]+""".toRegex(RegexOption.IGNORE_CASE)
 
     fun process(message: String): String {
-        val escaped = StringEscapeUtils.escapeHtml4(message)
+        @Suppress("VARIABLE_WITH_REDUNDANT_INITIALIZER") var escaped = message
 
-        return escaped.replace(URL_PATTERN) { "<a href=\"${it.value}\" target=\"_blank\">${it.value}</a>" }
+        // linkify messages
+        escaped = StringEscapeUtils.escapeHtml4(message)
+        escaped = escaped.replace(URL_PATTERN) { "<a href=\"${it.value}\" target=\"_blank\">${it.value}</a>" }
+
+        // properly show multi-line messages
+        escaped = escaped.replace("\n", "<br />")
+
+        return escaped
     }
 }
