e2b6be83d54fc8ecb7bc4d8df9ba5e35496a64a8
Paul Staroch <paulchen@rueckgr.at>
2023-03-26 23:35:35 +0200

    update backend dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.ByteArrayOutputStream
 
-val ktorVersion = "2.2.3"
+val ktorVersion = "2.2.4"
 val log4jVersion = "2.20.0"
 val jacksonVersion = "2.14.2"
 
@@ -50,7 +50,7 @@ dependencies {
     implementation("io.ktor:ktor-client-auth:$ktorVersion")
     implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
-    implementation("org.slf4j:slf4j-api:2.0.6")
+    implementation("org.slf4j:slf4j-api:2.0.7")
     implementation("org.apache.logging.log4j:log4j-slf4j2-impl:$log4jVersion")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:$jacksonVersion")
     implementation("org.apache.commons:commons-lang3:3.12.0")
