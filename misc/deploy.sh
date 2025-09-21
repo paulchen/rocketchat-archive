692bc255bc8ba39a4500cd2adb8f82a9d09d902f
Paul Staroch <paulchen@rueckgr.at>
2025-09-22 01:40:18 +0200

    move build of images into containers

--- misc/deploy.sh
+++ misc/deploy.sh
@@ -4,13 +4,6 @@ SCRIPT_DIR=`dirname "$0"`
 SCRIPT_DIR=`realpath "$SCRIPT_DIR"`
 BASE_DIR=`realpath "$SCRIPT_DIR/.."`
 
-export JAVA_HOME=/usr/lib/jvm/java-21-openjdk/
-
-if [ ! -d "$JAVA_HOME" ]; then
-	echo "$JAVA_HOME not found"
-	exit 2
-fi
-
 cd "$SCRIPT_DIR"
 if [ ! -f deploy.conf ]; then
 	echo deploy.conf does not exist
@@ -28,25 +21,14 @@ if [ "$DEPLOY_HASH" != "`sha256sum misc/deploy.sh`" ]; then
 	exit $?
 fi
 
-. ~/.nvm/nvm.sh || exit 1
-nvm install lts/jod || exit 1
-
 docker pull debian:trixie-slim || exit 1
 docker pull eclipse-temurin:21-jdk || exit 1
 docker pull nginx:latest || exit 1
-
-cd "$BASE_DIR/frontend"
-
-npm install || exit 1
-ROCKETCHAT_URL="$ROCKETCHAT_URL" npm run build -- --base-href="$BASE_HREF" || exit 1
-
-cd "$BASE_DIR/backend"
-
-./gradlew distTar || exit 1
+docker pull node:22 || exit 1
 
 cd "$BASE_DIR"
 
-docker compose build --no-cache || exit 1
+ROCKETCHAT_URL="$ROCKETCHAT_URL" docker compose build --no-cache || exit 1
 
 if [ "$1" != "--no-systemd" ]; then
 	sudo systemctl restart "$SYSTEMD_UNIT" || exit 1
